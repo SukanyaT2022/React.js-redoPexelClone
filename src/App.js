7072e5fa8494b1d86f45053500996e381066aeaa
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-11-15 12:59:23 -0500

    add

--- src/App.js
+++ src/App.js
@@ -1,48 +1,51 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react';
 
 const App = () => {
-//declare state step 2
-const [data,setData] = useState()
+  //declare state step 2
+  const [data, setData] = useState();
+  const orientation = 'landscape'
 
-
-
-  var apiKey = "c19t5LBUeXItCyO5nb5bpds50rtTKwmX7xOeFa4PnEBN8jabAxpjQW6U";
-  var auth = "https://api.pexels.com/v1/search?query=";
-   //step 1 bring api
+  var apiKey = 'c19t5LBUeXItCyO5nb5bpds50rtTKwmX7xOeFa4PnEBN8jabAxpjQW6U';
+  //step 1 bring api
   //use chat gpt use api call to pexel api- keyword to find skeleton
-  
+
   const category = 'nature'; // Replace with the desired category
-  const apiUrl = `https://api.pexels.com/v1/search?query=${category}&per_page=10`;
-  
+  const apiUrl = `https://api.pexels.com/v1/search?query=${category}&&orientation=${orientation}&per_page=10`;
+
   // Make a GET request to the Pexels API
   //promise
-  fetch(apiUrl, {
-  method: 'GET',
-  headers: {
-  'Authorization': apiKey,
-  },
-  })
-  //handle promise
-  .then(response=>response.json())
-  .then(data=>setData(data.photos))
+  useEffect(()=>{
+    fetch(apiUrl, {
+      method: 'GET',
+      headers: {
+        Authorization: apiKey,
+      },
+    })
+      //handle promise
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.photos)
+        setData(data.photos)
+      });
+  },[])
 
-//step 3 above bring data inside data by using setData--data store inside datat state
-
-  
-
+  //step 3 above bring data inside data by using setData--data store inside datat state
 
   return (
-
     <div>
-{/* //step4 map data --open consoloe photo-src-small
+      <div className='parent'>
+      {/* //step4 map data --open consoloe photo-src-small
 data && if we have data run otherwise not run
 */}
-         {data && data.map((val)=>{
-    <img src={val.src.small}/>
-  })
-}
+        {data &&
+          data.map((val) => {
+            return(
+              <img src={val.src.small} alt='something went wrong'/>
+            )
+          })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
